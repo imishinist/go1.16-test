0484655668e41d1a3d01581de828156202d5fb26
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-12 23:49:09 +0900

    readdir

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	_ "embed"
+	"embed"
 	"fmt"
+	"io/fs"
+	"log"
 )
 
 //go:embed message.txt
 var message string
 
+//go:embed assets/html/*
+var assets embed.FS
+
 func main() {
 	fmt.Println(message)
+
+	htmlfiles, err := fs.ReadDir(assets, "assets/html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, html := range htmlfiles {
+		fmt.Printf("%q\n", html.Name())
+	}
 }
