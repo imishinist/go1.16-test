c378a68a60799fd3a8082e7c7561666ef7ca99f2
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-13 16:14:48 +0900

    create from embed file

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/fs"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 //go:embed message.txt
@@ -12,6 +14,26 @@ var message string
 
 //go:embed assets/html/*
 var assets embed.FS
+
+func writeEmbedFS(dir embed.FS) {
+	tmp, err := os.MkdirTemp("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer os.RemoveAll(tmp) // clean up
+
+	fmt.Println(tmp)
+
+	fs.WalkDir(dir, "assets", fs.WalkDirFunc(func(path string, d fs.DirEntry, err error) error {
+		cpath := filepath.Join(tmp, path)
+		if d.IsDir() {
+			os.Mkdir(cpath, 0755)
+		} else {
+			os.WriteFile(cpath, []byte(""), 0644)
+		}
+		return nil
+	}))
+}
 
 func main() {
 	fmt.Println(message)
@@ -23,4 +45,5 @@ func main() {
 	for _, html := range htmlfiles {
 		fmt.Printf("%q\n", html.Name())
 	}
+	writeEmbedFS(assets)
 }
