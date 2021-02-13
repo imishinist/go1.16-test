2a5e90baa3436a2404c09a25b48e356e930fc901
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-14 06:40:44 +0900

    use embet

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/imishinist/go116-test
 
 go 1.16
+
+require github.com/imishinist/embet v0.1.0
--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
+
+	"github.com/imishinist/embet"
 )
 
 //go:embed message.txt
@@ -14,41 +15,6 @@ var message string
 
 //go:embed assets/html/*
 var assets embed.FS
-
-var (
-	DirPerm = os.FileMode(0755)
-	FilePerm = os.FileMode(0644)
-)
-
-func WriteEmbedFiles(dir embed.FS, prefix, dest string) error {
-	walkFunc := func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		dpath := filepath.Join(dest, path)
-		if d.IsDir() {
-			if err := os.Mkdir(dpath, DirPerm); err != nil {
-				return err
-			}
-			return nil
-		}
-
-		// if file
-		data, err := fs.ReadFile(dir, path)
-		if err != nil {
-			return err
-		}
-		if err := os.WriteFile(dpath, data, FilePerm); err != nil {
-			return err
-		}
-		return nil
-	}
-	if err := fs.WalkDir(dir, prefix, walkFunc); err != nil {
-		return err
-	}
-	return nil
-}
 
 func writeEmbedFS(dir embed.FS) {
 	tmp, err := os.MkdirTemp("", "example")
@@ -58,10 +24,13 @@ func writeEmbedFS(dir embed.FS) {
 	defer os.RemoveAll(tmp) // clean up
 
 	fmt.Println(tmp)
-	if err := WriteEmbedFiles(dir, "assets", tmp); err != nil {
+	if err := embet.WriteEmbedFiles(dir, "assets", tmp); err != nil {
 		log.Fatal(err)
 	}
 }
+
+//go:embed message.txt
+var messageFile embed.FS
 
 func main() {
 	fmt.Println(message)
@@ -74,4 +43,8 @@ func main() {
 		fmt.Printf("%q\n", html.Name())
 	}
 	writeEmbedFS(assets)
+
+	if err := embet.WriteEmbedFiles(messageFile, "message.txt", "tmp"); err != nil {
+		log.Fatal(err)
+	}
 }
